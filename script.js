a789dfaf8c2c633febb196a6050d3e06c795c44c
sfernandezpalomo <sfernandezpalomo@uoc.edu>
2026-03-18 17:01:29 -0500

    creada clase Show para estructurar datos

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+class Show {
+  constructor(id, name, image, summary) {
+    this.id = id;
+    this.name = name;
+    this.image = image;
+    this.summary = summary;
+  }
+}
+
 async function fetchShows() {
   const response = await fetch("https://api.tvmaze.com/shows");
   const data = await response.json();
